02ce9d1c6c21cba19d79b0dd261e18070345fca6
DC <szdcboy@gmail.com>
2019-03-20 09:10:02 +0800

    refactor: 重构代码

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# [2.0.0](https://github.com/dcboy/yp-wx-payment/compare/v1.0.5...v2.0.0) (2019-03-17)
+
+
+
+## [1.0.5](https://github.com/dcboy/yp-wx-payment/compare/v1.0.4...v1.0.5) (2018-12-27)
+
+
+
+## [1.0.4](https://github.com/dcboy/yp-wx-payment/compare/v1.0.3...v1.0.4) (2018-11-04)
+
+
+
+## [1.0.2](https://github.com/dcboy/yp-wx-payment/compare/v1.0.1...v1.0.2) (2018-09-07)
+
+
+
+## 1.0.1 (2018-04-02)
+
+
+
--- test/index.js
+++ test/index.js
@@ -1,26 +1,27 @@
-const tenpay = require('../src/wx-payment');
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const WxPayment = require('../src/wx-payment');
 const fs = require('fs');
 const path = require('path');
+const assert = require('assert');
 
-const config = process.env.TESTER == 'travis' ? {
+const config = process.env.TESTER === 'travis' ? {
   appid: process.env.appid || '0',
   mchid: process.env.mchid || '0',
   partnerKey: process.env.partnerKey || '0',
-  openid: process.env.openid || '0'
+  openid: process.env.openid || '0',
 } : require('../config');
 
-console.log(config);
 const pfxFile = path.resolve(__dirname, '../config/apiclient_cert.p12');
 if (fs.existsSync(pfxFile)) {
-  config.pfx = fs.readFileSync(pfxFile)
+  config.pfx = fs.readFileSync(pfxFile);
 }
 
-const api = new tenpay(config, true);
+const api = new WxPayment(config, true);
 
-const assert = require('assert');
 describe('订单相关', () => {
-  let id = Date.now();
-  let notify_url = 'http://d9b274dd.ngrok.io';
+  const id = Date.now();
+  const notify_url = 'http://d9b274dd.ngrok.io';
 
   // it('小微商户-申请入驻', async () => {
   //   let res = await api.applymentMicroSubmit({
@@ -31,28 +32,29 @@ describe('订单相关', () => {
   // });
 
   // it('获取证书', async () => {
-  //   let res = await api.getCertficates();
+  //   const res = await api.getCertficates();
   //   console.log(res);
-  //   assert.ok(res.return_code === 'SUCCESS');
+  //   assert.ok(res.serial_no);
   // });
 
   // it('获取unionid', async () => {
-  //   let res = await api.queryUnionID({
-  //     out_trade_no: 'OD181225154754142602',
+  //   const res = await api.queryUnionID({
+  //     out_trade_no: 'OD190313141329523958',
   //     openid: config.openid,
   //     mch_id: config.mchid,
+  //     sub_mch_id: config.sub_mchid,
   //   });
   //   console.log(res);
   //   assert.ok(res.return_code === 'SUCCESS');
   // });
 
   // it('刷脸authinfo', async () => {
-  //   let res = await api.facepayAuthInfo({
+  //   const res = await api.facepayAuthInfo({
   //     store_id: '123',
   //     store_name: '123',
   //     device_id: '123',
   //     rawdata: '3bvvkL2zeNNKpX0hloVk09wmvjGMVl2Pe7F7uU795FvHMxmWFqosNGDFUlxtTxdnYxUJWH+3aIsWq+ab1qltSkJzxhVgVEdGE/+xbUNn9hMacjdY4Ji3aDl1l8nC1OM9lb8jP9S2/xJR9o1Bk+8M1uLmS7iohFAZpEzgRetgs5GWFp/E1anibLfY3zkfLsbwkZuNbDUH2zo/n9yTHfm20QFwxfpWWswML0W77imqJFTABJlgHWnYZUnK+mtixKgTwpKdv4qwz9IfSsAsOfDiK1B+xa/iFfE+v8PgyYv7alGvj+kY83YVQqQ5hZzU/pr/HbAEpKam8QqRiPz3Y8Q3XkI2SjFM1g/uJdmFHS8EatGIWtshkczVrlkeWNntAnjb/jMoeCAKQoEYRl6SdHVbQbxqfArQKXe/gADWC1bmDvU2sa4cA4gwOXGUyw==',
-  //     // sub_mch_id: config.sub_mchid,
+  //     sub_mch_id: config.sub_mchid,
   //     mch_id: config.mchid,
   //     now: Math.round((new Date()).getTime() / 1000),
   //   });
@@ -60,7 +62,7 @@ describe('订单相关', () => {
   //   assert.ok(res.return_code === 'SUCCESS');
   // });
 
-  /** 
+  /**
 { return_code: 'SUCCESS',
   return_msg: 'OK',
   appid: 'wx7a0adc4aeeb295b7',
@@ -97,179 +99,181 @@ describe('订单相关', () => {
   //   assert.ok(res.result_code === 'SUCCESS');
   // });
 
-  // it.skip('H5/小程序支付参数(自动下单): getPayParams', async () => {
-  //   let res = await api.getPayParams({
-  //     out_trade_no: id,
-  //     body: '商品简单描述',
-  //     total_fee: 100,
-  //     notify_url: notify_url,
-  //     openid: config.openid
-  //   });
-  //   console.log('getPayParams', res);
-  //   let keys = ['appId', 'timeStamp', 'nonceStr', 'package', 'signType', 'paySign', 'timestamp'];
-  //   assert.deepEqual(Object.keys(res), keys);
-  // });
+  it.skip('H5/小程序支付参数(自动下单): getPayParams', async () => {
+    const res = await api.getPayParams({
+      out_trade_no: id,
+      body: '商品简单描述',
+      total_fee: 100,
+      notify_url,
+      sub_mch_id: config.sub_mchid,
+      openid: config.openid,
+    });
+    console.log('getPayParams', res);
+    const keys = ['appId', 'timeStamp', 'nonceStr', 'package', 'signType', 'paySign', 'timestamp'];
+    assert.deepEqual(Object.keys(res), keys);
+  });
 
-  // it.skip('APP支付参数(自动下单): getAppParams', async () => {
-  //   let res = await api.getAppParams({
-  //     out_trade_no: id,
-  //     body: '商品简单描述',
-  //     total_fee: 100,
-  //     notify_url: notify_url,
-  //   });
-  //   let keys = ['appid', 'partnerid', 'prepayid', 'package', 'noncestr', 'timestamp', 'sign'];
-  //   assert.deepEqual(Object.keys(res), keys);
-  // });
+  it.skip('APP支付参数(自动下单): getAppParams', async () => {
+    const res = await api.getAppParams({
+      out_trade_no: id,
+      body: '商品简单描述',
+      total_fee: 100,
+      sub_mch_id: config.sub_mchid,
+      notify_url,
+    });
+    const keys = ['appid', 'partnerid', 'prepayid', 'package', 'noncestr', 'timestamp', 'sign'];
+    assert.deepEqual(Object.keys(res), keys);
+  });
 
-  // it('统一下单: unifiedOrder', async () => {
-  //   let res = await api.unifiedOrder({
-  //     out_trade_no: id,
-  //     body: '商品简单描述',
-  //     total_fee: 100,
-  //     notify_url: notify_url,
-  //     openid: config.openid,
-  //     sub_mch_id: config.sub_mchid,
-  //   });
-  //   console.log('unifiedOrder', res);
-  //   assert.ok(res.return_code === 'SUCCESS');
-  //   assert.ok(res.result_code === 'SUCCESS');
-  // });
+  it.skip('统一下单: unifiedOrder', async () => {
+    const res = await api.unifiedOrder({
+      out_trade_no: id,
+      body: '商品简单描述',
+      total_fee: 100,
+      notify_url,
+      openid: config.openid,
+      sub_mch_id: config.sub_mchid,
+    });
+    console.log('unifiedOrder', res);
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 
-  // it('H5/小程序支付参数: getPayParamsByPrepay', async () => {
-  //   let res = await api.getPayParamsByPrepay({
-  //     prepay_id: id
-  //   });
-  //   console.log('getPayParamsByPrepay', res);
-  //   let keys = ['appId', 'timeStamp', 'nonceStr', 'package', 'signType', 'paySign', 'timestamp'];
-  //   assert.deepEqual(Object.keys(res), keys);
-  // });
+  it.skip('H5/小程序支付参数: getPayParamsByPrepay', async () => {
+    const res = await api.getPayParamsByPrepay({
+      prepay_id: id,
+    });
+    console.log('getPayParamsByPrepay', res);
+    const keys = ['appId', 'timeStamp', 'nonceStr', 'package', 'signType', 'paySign', 'timestamp'];
+    assert.deepEqual(Object.keys(res), keys);
+  });
 
-  // it.skip('APP支付参数: getAppParamsByPrepay', async () => {
-  //   let res = await api.getAppParamsByPrepay({
-  //     prepay_id: id
-  //   });
-  //   let keys = ['appid', 'partnerid', 'prepayid', 'package', 'noncestr', 'timestamp', 'sign'];
-  //   assert.deepEqual(Object.keys(res), keys);
-  // });
+  it.skip('APP支付参数: getAppParamsByPrepay', async () => {
+    const res = await api.getAppParamsByPrepay({
+      prepay_id: id,
+    });
+    const keys = ['appid', 'partnerid', 'prepayid', 'package', 'noncestr', 'timestamp', 'sign'];
+    assert.deepEqual(Object.keys(res), keys);
+  });
 
-  // it('生成扫码支付(模式一)URL: getNativeUrl', async () => {
-  //   let url = api.getNativeUrl({
-  //     product_id: '88888'
-  //   });
-  //   console.log('getNativeUrl', url);
-  //   assert.ok(url);
-  // });
+  it.skip('生成扫码支付(模式一)URL: getNativeUrl', async () => {
+    const url = api.getNativeUrl({
+      product_id: '88888',
+    });
+    console.log('getNativeUrl', url);
+    assert.ok(url);
+  });
 
-  // it('订单查询: orderQuery', async () => {
-  //   let res = await api.orderQuery({
-  //     out_trade_no: 'OD190313141329523958',
-  //     sub_mch_id: config.sub_mchid,
-  //   });
-  //   console.log('orderQuery', res);
-  //   assert.ok(res.return_code === 'SUCCESS');
-  //   assert.ok(res.result_code === 'SUCCESS');
-  // });
+  it.skip('订单查询: orderQuery', async () => {
+    const res = await api.orderQuery({
+      out_trade_no: 'OD190313141329523958',
+      sub_mch_id: config.sub_mchid,
+    });
+    console.log('orderQuery', res);
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 
-  // it('关闭订单: closeOrder', async () => {
-  //   let res = await api.closeOrder({
-  //     out_trade_no: id
-  //   });
-  //   console.log('closeOrder', res);
-  //   assert.ok(res.return_code === 'SUCCESS');
-  //   assert.ok(res.result_code === 'SUCCESS');
-  // });
+  it.skip('关闭订单: closeOrder', async () => {
+    const res = await api.closeOrder({
+      out_trade_no: id,
+    });
+    console.log('closeOrder', res);
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 });
 
 describe('退款相关', () => {
-  // it('申请退款: refund', async () => {
-  //   let res = await api.refund({
-  //     out_trade_no: 'OD190313141329523958',
-  //     out_refund_no: 'OD190313141329523958',
-  //     sub_mch_id: config.sub_mchid,
-  //     total_fee: 10,
-  //     refund_fee: 10
-  //   });
-  //   assert.ok(res.return_code === 'SUCCESS');
-  //   assert.ok(res.result_code === 'SUCCESS');
-  // });
+  it('申请退款: refund', async () => {
+    const res = await api.refund({
+      out_trade_no: 'OD190313141329523958',
+      out_refund_no: 'OD190313141329523958',
+      sub_mch_id: config.sub_mchid,
+      total_fee: 10,
+      refund_fee: 10,
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 
-  //   it.skip('退款查询: refundQuery - out_trade_no', async () => {
-  //     let res = await api.refundQuery({
-  //       out_trade_no: '1711185583256741'
-  //     });
-  //     assert.ok(res.return_code === 'SUCCESS');
-  //     assert.ok(res.result_code === 'SUCCESS');
-  //   });
+  it.skip('退款查询: refundQuery - out_trade_no', async () => {
+    const res = await api.refundQuery({
+      out_trade_no: '1711185583256741',
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 
-  //   it.skip('退款查询: refundQuery - out_refund_no', async () => {
-  //     let res = await api.refundQuery({
-  //       out_refund_no: 'REFUND_1711185583256741'
-  //     });
-  //     assert.ok(res.return_code === 'SUCCESS');
-  //     assert.ok(res.result_code === 'SUCCESS');
-  //   });
+  it.skip('退款查询: refundQuery - out_refund_no', async () => {
+    const res = await api.refundQuery({
+      out_refund_no: 'REFUND_1711185583256741',
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 });
 
-// describe('企业付款相关', () => {
-//   let id = 'T1514732081550';
+describe('企业付款相关', () => {
+  const id = 'T1514732081550';
 
-//   it.skip('申请付款: transfers', async () => {
-//     let res = await api.transfers({
-//       partner_trade_no: id,
-//       openid: config.openid,
-//       amount: 100,
-//       desc: '企业付款测试'
-//     });
-//     assert.ok(res.return_code === 'SUCCESS');
-//     assert.ok(res.result_code === 'SUCCESS');
-//   });
+  it.skip('申请付款: transfers', async () => {
+    const res = await api.transfers({
+      partner_trade_no: id,
+      openid: config.openid,
+      amount: 100,
+      desc: '企业付款测试',
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 
-//   it.skip('付款查询: transfersQuery', async () => {
-//     let res = await api.transfersQuery({
-//       partner_trade_no: id
-//     });
-//     assert.ok(res.return_code === 'SUCCESS');
-//     assert.ok(res.result_code === 'SUCCESS');
-//   });
-// });
+  it.skip('付款查询: transfersQuery', async () => {
+    const res = await api.transfersQuery({
+      partner_trade_no: id,
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
+});
 
-// describe('红包相关', () => {
-//   let mch_billno;
-//   it.skip('普通红包: sendRedpack', async () => {
-//     let res = await api.sendRedpack({
-//       mch_autono: Math.random().toString().substr(2, 10),
-//       send_name: '商户名称',
-//       re_openid: config.openid,
-//       total_amount: 100,
-//       wishing: '大吉大利，今晚吃鸡',
-//       act_name: '测试红包',
-//       remark: '无'
-//     });
-//     assert.ok(res.return_code === 'SUCCESS');
-//     assert.ok(res.result_code === 'SUCCESS');
-//     mch_billno = res.mch_billno;
-//   });
+describe('红包相关', () => {
+  let mch_billno;
+  it.skip('普通红包: sendRedpack', async () => {
+    const res = await api.sendRedpack({
+      mch_autono: Math.random().toString().substr(2, 10),
+      send_name: '商户名称',
+      re_openid: config.openid,
+      total_amount: 100,
+      wishing: '大吉大利，今晚吃鸡',
+      act_name: '测试红包',
+      remark: '无',
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+    ({ mch_billno } = res);
+  });
 
-//   it.skip('分裂红包: sendGroupRedpack', async () => {
-//     let res = await api.sendGroupRedpack({
-//       mch_autono: Math.random().toString().substr(2, 10),
-//       send_name: '商户名称',
-//       re_openid: config.openid,
-//       total_amount: 300,
-//       total_num: 3,
-//       wishing: '大吉大利，今晚吃鸡',
-//       act_name: '测试红包',
-//       remark: '无'
-//     });
-//     assert.ok(res.return_code === 'SUCCESS');
-//     assert.ok(res.result_code === 'SUCCESS');
-//   });
+  it.skip('分裂红包: sendGroupRedpack', async () => {
+    const res = await api.sendGroupRedpack({
+      mch_autono: Math.random().toString().substr(2, 10),
+      send_name: '商户名称',
+      re_openid: config.openid,
+      total_amount: 300,
+      total_num: 3,
+      wishing: '大吉大利，今晚吃鸡',
+      act_name: '测试红包',
+      remark: '无',
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
 
-//   it.skip('红包查询: redpackQuery', async () => {
-//     let res = await api.redpackQuery({
-//       mch_billno: config.mchid + '201801028986462339'
-//     });
-//     assert.ok(res.return_code === 'SUCCESS');
-//     assert.ok(res.result_code === 'SUCCESS');
-//   });
-// });
+  it.skip('红包查询: redpackQuery', async () => {
+    const res = await api.redpackQuery({
+      mch_billno: `${config.mchid}201801028986462339`,
+    });
+    assert.ok(res.return_code === 'SUCCESS');
+    assert.ok(res.result_code === 'SUCCESS');
+  });
+});
